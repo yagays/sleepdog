9d8067095c7725c8c56b40652ea34f176b330764
yag_ays <yanagi.ayase@gmail.com>
2017-08-19 15:35:20 +0900

    add support for any filetype

--- sleepdog.py
+++ sleepdog.py
@@ -12,7 +12,7 @@ from watchdog.observers import Observer
 from slacker import Slacker
 
 
-class CreateJsonHandler(FileSystemEventHandler):
+class CreateFileHandler(FileSystemEventHandler):
 
     def __init__(self, pattern, channel, token):
         self.pattern = pattern
@@ -23,11 +23,18 @@ class CreateJsonHandler(FileSystemEventHandler):
         if event.is_directory:
             return
         if self.pattern in event.src_path:
-            with open(event.src_path) as f:
-                output = json.load(f)
-                message = "```" + json.dumps(output, indent=4, sort_keys=True) + "```"
-                slack = Slacker(self.token)
-                slack.chat.post_message(self.channel, message)
+            message = ""
+            if os.path.splitext(os.path.basename(event.src_path))[1] == ".json":
+                with open(event.src_path) as f:
+                    output = json.load(f)
+                    message = json.dumps(output, indent=4, sort_keys=True)
+            else:
+                with open(event.src_path) as f:
+                    message = f.read()
+
+            message = "```" + message + "```"
+            slack = Slacker(self.token)
+            slack.chat.post_message(self.channel, message)
 
 
 if __name__ in "__main__":
@@ -41,7 +48,7 @@ if __name__ in "__main__":
     args = parser.parse_args()
 
     while True:
-        event_handler = CreateJsonHandler(args.pattern, args.channel, args.token)
+        event_handler = CreateFileHandler(args.pattern, args.channel, args.token)
         observer = Observer()
         observer.schedule(event_handler, args.dir, recursive=True)
         observer.start()
